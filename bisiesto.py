5480443f44f33df159082559fe7a6a251a74395d
root <root@localhost.localdomain>
2018-01-25 13:38:12 +0100

    first commit

--- bisiesto.py
+++ bisiesto.py
@@ -1,6 +1,9 @@
-anio=int(input("Escribe un anio: "))
-
-if anio%4==0 and anio%100!=0 or anio%400==0:
-    print ("Bisiesto")
-else:
-    print ("No es")
+def esbisiesto(anio):
+    if anio%4==0 and anio%100!=0 or anio%400==0:
+        print ("Bisiesto")
+    else:
+        print ("No es bisiesto")
+print("Iniciando...")
+esbisiesto(2100)
+esbisiesto(2300)
+esbisiesto(2020)
